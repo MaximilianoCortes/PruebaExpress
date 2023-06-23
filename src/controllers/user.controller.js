f0cfc0ba1b1abce5d7f9810224d2d6d7611319e3
MaximilianoCortes <m.cortes12@ufromail.cl>
2023-06-23 15:49:02 -0400

    hagan commits

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -6,7 +6,6 @@ async function createUser(req, res) {
         
         const name = req.body.name;
         const email = req.body.email;
-        const dni = req.body.dni;
         const password = req.body.password;
         const encryptedPassword = bcrypt.hashSync(password, 10);
 
